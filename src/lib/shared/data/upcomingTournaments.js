4ac508ecec155d481ca08f85e3ee6c47e5cc54ce
DarkDev <darkdev_brand@riseup.net>
2024-09-02 21:37:36 +0300

    alfa v.

---
+++ src/lib/shared/data/upcomingTournaments.js
@@ -0,0 +1,25 @@
+const upcomingTournaments = [
+    {
+      place: 'Trubadurów 8, Gdansk',
+      time: '19:20',
+      date: '17 września.'
+    },
+    {
+      place: 'Trubadurów 8, Gdansk',
+      time: '19:20',
+      date: '01 października.'
+    },
+    {
+      place: 'Trubadurów 8, Gdansk',
+      time: '19:20',
+      date: '15 października.'
+    },
+    {
+      place: 'Trubadurów 8, Gdansk',
+      time: '19:20',
+      date: '5 listopada.'
+    },
+];
+
+
+export default upcomingTournaments;
