ad163f4e723202ec559c3d870fa08ba5ebf79cf4
DarkDev <darkdev_brand@riseup.net>
2024-09-02 21:39:58 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Sponsors.svelte
@@ -0,0 +1,46 @@
+<script>
+import {PatronStart} from '$entities'
+</script>
+
+<section class="section_sponsor-wp2024">
+  <div class="padding-global padding-section-medium">
+    <div class="container-large">
+      <a name="sponsors"></a>
+
+      <h2  class="heading-wp2024">Sponsorzy</h2>
+      <div class="spacer-large"></div>
+      <div  class="sponsor-wp2024_list">
+        <div  class="sponsor-wp2024_item is-you-logo">
+          <div class="sponsor-wp2024_logo w-embed">
+
+          </div>
+        </div>
+
+        <div class="sponsor-wp2024_item is-middle">
+          <div class="sponsor-wp2024_logo w-embed">
+            <img src="/images/logos/darkdev_turq.svg" alt="">
+          </div>
+        </div>
+
+
+        <PatronStart />
+
+      </div>
+      <div class="spacer-large"></div>
+      <button data-w-id="42c7151f-b168-fff9-28fa-955e20d8a45d" aria-label="Ouvrir le formulaire pour sponsoriser l'événement"  class="sponsor-wp2024_button">
+        <div class="text-size-large">jak zostać sponsorem?</div>
+        <div class="icon-embed-small w-embed">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 32 32" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+            <path d="M7.78984 22.0457L18.7132 11.1223L10.2279 11.1223L10.2279 8.45608L23.2651 8.45608L23.2651 21.4932H20.5988L20.5988 13.008L9.67545 23.9313L7.78984 22.0457Z" fill="currentColor"></path>
+          </svg>
+        </div>
+        <div class="wp2024-overlay"></div>
+      </button>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
