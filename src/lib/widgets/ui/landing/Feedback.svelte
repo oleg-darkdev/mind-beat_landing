ea18e8e413c1ca94179942489b237782a7b145e2
DarkDev <darkdev_brand@riseup.net>
2024-09-02 21:39:49 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Feedback.svelte
@@ -0,0 +1,103 @@
+<section id="images" class="section_gallery-wp2024">
+    <div class="padding-global padding-section-medium">
+
+      <a name="report"></a>
+
+      <div class="container-large">
+        <h2 animate="from-bottom" class="heading-wp2024">Opinie</h2>
+        <div class="spacer-large"></div>
+        <div animate="from-bottom" class="gallery-wp2024_component">
+          <div class="gallery-wp2024_testi-wrapper splide">
+            <div class="splide__track w-dyn-list">
+              <div role="list" class="gallery-wp2024_testi-list splide__list w-dyn-items">
+                <div role="listitem" class="gallery-wp2024_testi-item splide__slide w-dyn-item">
+                  <div class="gallery-wp2024_testi-author">
+                    <img src="images/66bb420ee6093e6b173a0398_philippe-marx.webp" loading="lazy" width="48" height="48" alt="" class="gallery-wp2024_testi-author-image">
+                    <div class="gallery-wp2024_testi-author-infos">
+                      <div class="text-weight-medium"> the printing and</div>
+                      <div class="text-color-neutral-wp2024">Lorem Ipsum is simply dummy text of</div>
+                    </div>
+                  </div>
+                  <p class="text-size-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <!-- <a  href="https://fr.linkedin.com/posts/philippe-marx_webflow-la-solution-tout-en-un-pour-la-cr%C3%A9ation-activity-7176213503282708481-PPNB" target="_blank" class="gallery-wp2024_testi-link w-inline-block">
+                    <div class="icon-embed-xxsmall w-embed">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 16 16" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+                        <path d="M3.44 4.89001C4.24 4.89001 4.88 4.24001 4.88 3.45001C4.88 2.66001 4.23 2.01001 3.44 2.01001C2.65 2.01001 2 2.66001 2 3.45001C2 4.24001 2.65 4.89001 3.44 4.89001ZM6.25 5.98001V14H8.73V10.04C8.73 8.99001 8.93 7.98001 10.22 7.98001C11.51 7.98001 11.51 9.18001 11.51 10.1V14H14V9.60001C14 7.44001 13.54 5.78001 11.02 5.78001C9.81 5.78001 9 6.44001 8.67 7.07001H8.64V5.97001H6.26L6.25 5.98001ZM2.2 5.98001H4.69V14H2.2V5.98001Z" fill="currentColor"></path>
+                      </svg>
+                    </div>
+                  </a> -->
+                </div>
+              </div>
+            </div>
+
+
+            <div id="w-node-_64721ac8-7fe9-ca70-22cb-f57e3f75a853-ff719c49" class="splide__arrows">
+              <button aria-label="Voir le testimonial précédent" class="splide__arrow splide__arrow--prev">
+                <div class="icon-embed-xsmall w-embed">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+                    <path d="M19.706 12.7921L8.11999 12.7921L12.62 17.2921L11.206 18.7061L4.29199 11.7921L11.206 4.87805L12.62 6.29205L8.11999 10.7921L19.706 10.7921L19.706 12.7921Z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </button>
+              <button aria-label="Voir le testimonial suivant" class="splide__arrow splide__arrow--next">
+                <div class="icon-embed-xsmall w-embed">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+                    <path d="M4.29401 11.2079L15.88 11.2079L11.38 6.70795L12.794 5.29395L19.708 12.2079L12.794 19.1219L11.38 17.7079L15.88 13.2079L4.29401 13.2079L4.29401 11.2079Z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </div>
+          <!-- images/66bb494a003eae67ca18afa3_video-webflow-watch-party-paris-2023.webp -->
+          <a href="#" class="video-wrap gallery-wp2024_lightbox-link w-inline-block w-lightbox">
+            <img src="" loading="lazy" width="1280" height="720" alt="thumbnail du récap vidéo de la WWP 2023" srcset="images/66bb494a003eae67ca18afa3_video-webflow-watch-party-paris-2023-p-500.webp 500w, images/66bb494a003eae67ca18afa3_video-webflow-watch-party-paris-2023-p-800.webp 800w, images/66bb494a003eae67ca18afa3_video-webflow-watch-party-paris-2023-p-1080.webp 1080w, images/66bb494a003eae67ca18afa3_video-webflow-watch-party-paris-2023.webp 1280w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 94vw, (max-width: 991px) 89vw, 57vw" class="gallery-wp2024_lightbox-image">
+            <div class="icon-embed-medium z-index-2 w-embed">
+              <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 47 32" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+                <g clip-path="url(#clip0_62_118)">
+                  <path d="M45.2172 4.99111C44.9561 4.02641 44.4469 3.14692 43.7403 2.44023C43.0336 1.73353 42.1541 1.22429 41.1894 0.9632C37.6577 0 23.4437 0 23.4437 0C23.4437 0 9.2289 0.0291555 5.69717 0.992356C4.73246 1.25346 3.85298 1.76273 3.14632 2.46946C2.43965 3.17619 1.93045 4.05571 1.66944 5.02044C0.601171 11.2956 0.186771 20.8576 1.69877 26.8818C1.95982 27.8465 2.46902 28.726 3.17569 29.4327C3.88235 30.1394 4.76182 30.6486 5.7265 30.9097C9.25824 31.8729 23.4726 31.8729 23.4726 31.8729C23.4726 31.8729 37.6869 31.8729 41.2184 30.9097C42.1831 30.6486 43.0626 30.1394 43.7693 29.4327C44.476 28.726 44.9853 27.8465 45.2463 26.8818C46.3731 20.5977 46.7203 11.0416 45.2172 4.99111Z" fill="#FF0000"></path>
+                  <path d="M18.9196 22.7663L30.7112 15.9364L18.9196 9.10657V22.7663Z" fill="#FDFDFD"></path>
+                </g>
+                <defs>
+                  <clipPath id="clip0_62_118">
+                    <rect width="45.5111" height="32" fill="white" ></rect>
+                  </clipPath>
+                </defs>
+              </svg>
+            </div>
+          </a>
+
+
+          <div class="gallery-wrap gallery-wp2024_gallery-wrapper splide">
+            <div class="splide__track w-dyn-list">
+              <div role="list" class="gallery-wp2024_list splide__list w-dyn-items">
+
+                <div role="listitem" class="gallery-wp2024_gallery_item splide__slide w-dyn-item">
+                  <img src="images/66bc6acd697b4fce73c12930_networking-wwp-2024.webp" loading="lazy" width="1500" height="1000" alt="" sizes="(max-width: 479px) 91vw, (max-width: 767px) 94vw, (max-width: 991px) 89vw, 34vw" class="gallery-wp2024_gallery-image">
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <style>
+.gallery-wrap {
+  grid-area: 1/2/3/3;
+}
+.video-wrap {
+  grid-area: 2/1/3/2;
+  align-self: stretch;
+}
+
+@media screen and (max-width: 991px) {
+  .gallery-wrap {
+    grid-area: span 2 / span 1 / span 2 / span 1;
+  }
+  .video-wrap {
+    grid-area: span 1 / span 1 / span 1 / span 1;
+  }
+}
+  </style>
