1ac38cf662a4e73ff1262a3efb4d1b22bc366ae5
DarkDev <darkdev_brand@riseup.net>
2024-09-02 21:39:12 +0300

    beta v.

---
+++ src/lib/widgets/ui/landing/Footer.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<footer class="footer-wp2024_component">
+  <div class="padding-global padding-section-small">
+    <div class="container-large">
+      <div class="footer-wp2024_content">
+        <div class="text-weight-medium">© MIND+BEAT 2024</div>
+        <a href="https://oleg-darkdev.vercel.app/" target='_blank' class="footer-wp2024_link">Development by <span class='text-[#2377f5]'>oleg.darkdev</span></a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<style>
+
+</style>
