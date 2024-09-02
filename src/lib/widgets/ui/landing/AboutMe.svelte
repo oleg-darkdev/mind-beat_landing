8f6ab10c1032ae293f22c112f99b0a831f1f5328
DarkDev <darkdev_brand@riseup.net>
2024-09-02 21:43:46 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/AboutMe.svelte
@@ -0,0 +1,49 @@
+<script>
+
+
+</script>
+
+
+<section  class="section_inscription-wp2024">
+  <div class="padding-global padding-section-medium padding-bottom-huge">
+    <a name="contact"></a>
+    <div class="container-medium">
+      <div class="text-align-center">
+        <h2 animate="from-bottom" class="heading-wp2024 z-index-2">Sędzia i organizator</h2>
+      </div>
+      <div class="spacer-xlarge"></div>
+      <a href="/" class="inscription-wp2024_component w-inline-block">
+        <div class="inscription-wp2024_content-top">
+          <div class="icon-embed-medium w-embed">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 48 48" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+              <path d="M6.36731 8.19457C6.36731 4.94849 8.99878 2.31702 12.2449 2.31702H35.7551C39.0011 2.31702 41.6326 4.94849 41.6326 8.19457V39.5415C41.6326 42.7876 39.0011 45.419 35.7551 45.419H12.2449C8.99878 45.419 6.36731 42.7876 6.36731 39.5415V8.19457Z" stroke="#146EF5" stroke-width="1.46939"></path>
+              <path d="M12.2449 8.19453V7.45984C11.8392 7.45984 11.5103 7.78878 11.5103 8.19453H12.2449ZM35.7552 8.19453H36.4898C36.4898 7.78878 36.1609 7.45984 35.7552 7.45984V8.19453ZM35.7552 39.5414V40.2761C36.1609 40.2761 36.4898 39.9472 36.4898 39.5414H35.7552ZM12.2449 39.5414H11.5103C11.5103 39.9472 11.8392 40.2761 12.2449 40.2761V39.5414ZM12.2449 8.92923H35.7552V7.45984H12.2449V8.92923ZM35.0205 8.19453V39.5414H36.4898V8.19453H35.0205ZM12.9796 39.5414V8.19453H11.5103V39.5414H12.9796ZM12.2449 40.2761H35.7552V38.8067H12.2449V40.2761Z" fill="#ABABAB"></path>
+              <path d="M21.5769 14.072L22.3039 13.9655C22.2511 13.6047 21.9416 13.3373 21.5769 13.3373V14.072ZM18.1224 14.072V13.3373C17.7166 13.3373 17.3877 13.6662 17.3877 14.072H18.1224ZM26.423 14.072V13.3373C26.0583 13.3373 25.7488 13.6047 25.696 13.9655L26.423 14.072ZM29.8775 14.072H30.6122C30.6122 13.6662 30.2832 13.3373 29.8775 13.3373V14.072ZM29.8775 33.6638V34.3985C30.2832 34.3985 30.6122 34.0696 30.6122 33.6638H29.8775ZM26.3694 33.6638L25.6586 33.8497C25.7431 34.173 26.0352 34.3985 26.3694 34.3985V33.6638ZM21.6305 33.6638V34.3985C21.9646 34.3985 22.2568 34.173 22.3413 33.8497L21.6305 33.6638ZM18.1224 33.6638H17.3877C17.3877 34.0696 17.7166 34.3985 18.1224 34.3985V33.6638ZM21.5769 13.3373H18.1224V14.8067H21.5769V13.3373ZM23.9999 15.4281C23.139 15.4281 22.4251 14.7928 22.3039 13.9655L20.8501 14.1786C21.0755 15.7168 22.3992 16.8975 23.9999 16.8975V15.4281ZM25.696 13.9655C25.5747 14.7928 24.8609 15.4281 23.9999 15.4281V16.8975C25.6007 16.8975 26.9244 15.7168 27.1498 14.1786L25.696 13.9655ZM29.8775 13.3373H26.423V14.8067H29.8775V13.3373ZM30.6122 33.6638V14.072H29.1428V33.6638H30.6122ZM26.3694 34.3985H29.8775V32.9291H26.3694V34.3985ZM23.9999 32.571C24.7954 32.571 25.466 33.1135 25.6586 33.8497L27.0802 33.478C26.7227 32.1111 25.4801 31.1016 23.9999 31.1016V32.571ZM22.3413 33.8497C22.5339 33.1135 23.2045 32.571 23.9999 32.571V31.1016C22.5198 31.1016 21.2772 32.1111 20.9197 33.478L22.3413 33.8497ZM18.1224 34.3985H21.6305V32.9291H18.1224V34.3985ZM17.3877 14.072V33.6638H18.8571V14.072H17.3877Z" fill="#FDFDFD"></path>
+              <path d="M18.1224 19.9496H29.8775" stroke="#FDFDFD" stroke-width="1.46939" stroke-linejoin="round" stroke-dasharray="1.47 1.47"></path>
+            </svg>
+          </div>
+          <div class="icon-embed-small w-embed">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 32 32" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+              <path d="M7.78984 22.0457L18.7132 11.1223L10.2279 11.1223L10.2279 8.45608L23.2651 8.45608L23.2651 21.4932H20.5988L20.5988 13.008L9.67545 23.9313L7.78984 22.0457Z" fill="currentColor"></path>
+            </svg>
+          </div>
+        </div>
+
+        
+        <div class="inscription-wp2024_content-bottom">
+          <h3 class="heading-wp2024 is-small">OLEG Dark.Dev</h3>
+          <p class="text-size-medium text-color-neutral-wp2024">Mistrz Białorusi w szachach.</p>
+        </div>
+        <div class="wp2024-overlay"></div>
+      </a>
+    </div>
+  </div>
+  <div class="inscription-wp2024_background"></div>
+  <div id="inscription-canvas" class="inscription-wp2024_canvas"></div>
+</section>
+
+
+
+<style>
+
+</style>
