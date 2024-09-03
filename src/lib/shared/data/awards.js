cb872ef402f777c5dfa6b8471f99d0b67ad53e8e
DarkDev <darkdev_brand@riseup.net>
2024-09-03 12:07:41 +0300

    refactoring data import, upd content

---
+++ src/lib/shared/data/awards.js
@@ -0,0 +1,33 @@
+const awards = [
+
+    {
+        img: '/images/medals/honors_second.svg',
+        title: '2 miejsce',
+        awards: [
+            'Dyplom',
+            'Bilet wejściowy do imprezki.'
+        ]
+    },
+    {
+        img: '/images/medals/honors_first.svg',
+        title: '1 miejsce',
+        awards: [
+            'Dyplom',
+            'Bilet wejściowy do imprezki.',
+            'Prezent od sponsorów.'
+
+        ],
+    },
+    {
+        img: '/images/medals/honors_third.svg',
+        title: '3 miejsce',
+        awards: [
+            'Dyplom',
+            'Bilet wejściowy do imprezki.'
+
+        ]
+    },
+];
+
+
+export default awards;
---
+++ src/lib/widgets/ui/landing/Awards.svelte
@@ -0,0 +1,49 @@
+<script>
+  import {awards} from '$shared'
+</script>
+
+
+
+<section  class="section_inscription-wp2024">
+  <div class="padding-global padding-section-medium padding-bottom-huge">
+    <!-- <a name=""></a> -->
+    <div class="container-medium">
+<div class="flex flex-row flex-wrap flex-wrap justify-center ">
+    {#each awards as award}
+
+        <div style="background-image: url({award.img}); " class="card-award m-2 -mt-10 items-end flex max-w-sm">
+            <div style="min-height: 200px; height: auto; width: 100%; " class="bg-[#111111] rounded-lg border border-[#146ef5] shadow-md flex max-w-sm flex-col p-4 sm:p-6">
+                <span class="text-6xl font-impact tracking-tight text-white text-center">
+                    {award.title}
+                </span>
+
+                <ul class="text-color-neutral-wp2024 max-w-md list-disc list-inside space-y-1">
+                    {#each award.awards as award}
+                    <li class="">
+                        {award}
+                    </li>
+                    {/each}
+                </ul>
+                <!-- {#if award.hackerspace}
+                <span class="text-1xl mt-6 tracking-tight text-white text-center">
+                    Serdeczne podziękowania dla  <a class="inline-flex items-center hover:underline font-normal text-light underline hover:no-underline" href="https://44hackerspace.vercel.app/">44Hackerspace</a> za udostępnienie sprzętu do druku 3D.
+                </span>
+
+                {/if} -->
+            </div>
+
+        </div>
+    {/each}
+</div>
+</div>
+</div>
+</section>
+
+<style>
+    .card-award {
+        height: 440px;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+    }
+</style>
