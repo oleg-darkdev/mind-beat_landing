f9e88a0d4ea5c548f5692680b0d3aa6d4af8ea03
DarkDev <darkdev_brand@riseup.net>
2024-09-03 12:07:47 +0300

    refactoring data import

---
+++ src/lib/shared/data/faq.js
@@ -0,0 +1,48 @@
+const faq = [
+  {
+    q: `Czym jest turniej szachowy MIND+BEAT?`,
+    a: [
+      `To wyjątkowe wydarzenie, w którym intelektualna gra w szachy łączy się z energią i atmosferą nadchodzącego rave'u. Turniej odbywa się przed rozpoczęciem imprezy, tworząc wyjątkowy nastrój dla wszystkich uczestników.`,
+      ``,
+    ]
+  },
+  {
+    q: `Dla kogo adresowane jest wydarzenie?`,
+    a: [
+      `Wydarzenie jest odpowiednie zarówno dla entuzjastów szachów, jak i tych, którzy po prostu chcą cieszyć się niezwykłym doświadczeniem przed imprezą.`,
+      `Poziom gry nie ma znaczenia, liczy się chęć uczestnictwa i dobra zabawa.`,
+    ]
+  },
+  {
+    q: `Jaka jest formuła turnieju?`,
+    a: [
+      `Turniej odbywa się w formacie szachów szybkich, z kontrolą czasu 10 minut na gracza.`,
+      ` Pozwala to na utrzymanie dynamiki i rozgrzewki przed turniejem.`,
+    ]
+  },
+  {
+    q: `Jakie nagrody czekają na zwycięzców?`,
+    a: [
+      `Zwycięzcy turnieju otrzymają bilety wstępu, ekskluzywne dyplomy i upominki od sponsorów.`
+    ]
+  },
+  {
+    q: `Jak zarejestrować się na turniej?`,
+    a: [
+      `Wstępna rejestracja jest obowiązkowa, ponieważ liczba uczestników jest ograniczona.`,
+      `Pomaga nam to lepiej przygotować turniej i upewnić się, że wszyscy czują się w nim komfortowo.`,
+      `Możesz zarejestrować się online i offline. Rejestracja kończy się 10 minut przed rozpoczęciem turnieju.`
+    ]
+  },
+  {
+    q: `Czy mogę wziąć udział w imprezie, jeśli nie gram w szachy?`,
+    a: [
+      `Oczywiście! Turniej jest tylko jedną z części wydarzenia.`,
+      `Jeśli nie chcesz brać udziału w szachach, nadal możesz przyjść na imprezę i cieszyć się muzyką i atmosferą.`,
+    ]
+  },
+
+]
+
+
+export default faq;
