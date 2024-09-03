25d6beeee91135962ed7cdff55f89db455b93109
DarkDev <darkdev_brand@riseup.net>
2024-09-03 13:13:25 +0300

    add location coordinates

--- src/lib/shared/data/upcomingTournaments.js
+++ src/lib/shared/data/upcomingTournaments.js
@@ -1,23 +1,27 @@
 const upcomingTournaments = [
     {
       place: 'Trubadurów 8, Gdansk',
-      time: '19:20',
-      date: '17 września.'
+      time: '19:10',
+      date: '17 września.',
+      location: 'https://maps.app.goo.gl/wVWCJXqA95MuZ1bU7'
     },
     {
       place: 'Trubadurów 8, Gdansk',
-      time: '19:20',
-      date: '01 października.'
+      time: '19:10',
+      date: '01 października.',
+      location: 'https://maps.app.goo.gl/wVWCJXqA95MuZ1bU7'
     },
     {
       place: 'Trubadurów 8, Gdansk',
-      time: '19:20',
-      date: '15 października.'
+      time: '19:10',
+      date: '15 października.',
+      location: 'https://maps.app.goo.gl/wVWCJXqA95MuZ1bU7'
     },
     {
       place: 'Trubadurów 8, Gdansk',
-      time: '19:20',
-      date: '5 listopada.'
+      time: '19:10',
+      date: '5 listopada.',
+      location: 'https://maps.app.goo.gl/wVWCJXqA95MuZ1bU7'
     },
 ];
 
