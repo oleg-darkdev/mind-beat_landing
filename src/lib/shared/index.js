7bbcfc10124633fb6e4ea684f347a36acc03bcec
DarkDev <darkdev_brand@riseup.net>
2024-09-03 13:12:00 +0300

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -1,8 +1,16 @@
+// data
 import upcomingTournaments from './data/upcomingTournaments';
 import faq from './data/faq';
 import agenda from './data/agenda';
 import awards from './data/awards';
-// import upcomingTournaments from './data/upcomingTournaments';
+import navigation from './data/navigation';
+// import awards from './data/awards';
+// import awards from './data/awards';
+// import awards from './data/awards';
 
 
-export { upcomingTournaments, faq, agenda, awards };
+// ui
+import RegistrationBtn from './ui/RegistrationBtn.svelte';
+
+
+export { upcomingTournaments, faq, agenda, awards, RegistrationBtn, navigation };
